5f32f5a64ac123b3d00bb64c1cb0e79ddba3ab74
Ann <ann.nguyen.codes@gmail.com>
2020-07-26 02:09:02 -0400

    include build for heroku

---
+++ build/precache-manifest.6a678540efb26883aa889cf14f549b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c585dd230804a1b83a018e8a0b1a7dff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e346f7192e7a5f86b4",
+    "url": "/static/css/main.3b7d17b0.chunk.css"
+  },
+  {
+    "revision": "2774c1da7c6864539e80",
+    "url": "/static/js/2.405e790e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.405e790e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e346f7192e7a5f86b4",
+    "url": "/static/js/main.804d4fb3.chunk.js"
+  },
+  {
+    "revision": "1830bfa02fb29a4baa30",
+    "url": "/static/js/runtime-main.beb7f13f.js"
+  }
+]);
